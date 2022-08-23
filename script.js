b18e472ab694c050d276f1b02916a1de6d644a4f
flaviajuchen <67715601+flaviajuchen@users.noreply.github.com>
2022-08-22 21:24:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function calcularmedia(a, b, c){
+    let resultado = (a + b + c)/3;
+    return resultado;
+}
+
+function calcularexame(nota){
+    let resultado = 10 - nota;
+    return resultado;
+}
+
+
 $('#inputOla').click(
     function(){
         calculo =
@@ -6,13 +17,18 @@ $('#inputOla').click(
             'b':document.getElementById('inputB').value,
             'c':document.getElementById('inputC').value
         }
-        let media = ( parseInt(calculo.a) + parseInt(calculo.b) + parseInt(calculo.c)) /3;
-        let exm = (10 - media);
+        //let media = ( parseInt(calculo.a) + parseInt(calculo.b) + parseInt(calculo.c)) /3;
+        //let exm = (10 - media);
+        let media = calcularmedia(
+            parseInt(calculo.a),
+            parseInt(calculo.b), 
+            parseInt(calculo.c)
+        );
         if 
         (media >= 7) {
         alert('Você foi aprovado');
          } else {
-            alert('Você esta de recuperação, você precisa de ' + exm);
+            alert('Você esta de recuperação, você precisa de ' + calcularexame(media));
          }
 
 }
